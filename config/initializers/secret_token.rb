6de0c3a8c87823de1d1d73e358f5598b243caf2b
Joris Nederpelt Lazarom <joris@12notes.nl>
2011-02-09 19:43:15 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Choosefairy::Application.config.secret_token = '7a7fe20bf8b97d671162721cf61c687f17942328f3b1e89d364b88d077e796cfa1aff35b8f3c69e82e7539265b4a3e0df73873b139e31042466d235e54ecd0d7'
